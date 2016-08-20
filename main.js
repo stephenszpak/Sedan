fe6818696776edcb3c3cf8258cd336f49be13bfc
Stephen Szpak <srszpak@gmail.com>
2016-08-20 10:10:10 -0500

    added augmented

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("main.js")
+
+
+console.log("Sedan IIFE", Sedan);
+console.log("Sedan Color", Sedan.getColor());
+console.log("Sedan Type", Sedan.getType());
+
+console.log("Sedan set Occupancy", Sedan.setOccupancy(5));
+console.log("Sedan set Occupancy", Sedan.setOccupancy());
